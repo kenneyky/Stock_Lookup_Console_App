980ca24b4ca52708186fb2ee2fa8b65c4486320f
kenneyky <kenneyky520@gmail.com>
2021-01-22 17:31:08 -0500

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Configuration;
+using System.Threading.Tasks;
+using IEXSharp;
+
+namespace Stock_Lookup_Console
+{
+    class Program
+    {
+        private async static Task GetStockAsync(string stockSymbol) {
+            var publishableToken = ConfigurationManager.AppSettings.Get("publishableToken");
+            var secretToken = ConfigurationManager.AppSettings.Get("secretToken");
+
+            IEXCloudClient client = new IEXCloudClient(publishableToken, secretToken, false, false);
+            try {
+                var response = await client.StockPrices.QuoteAsync(stockSymbol);
+                if (response.Data == null)
+                {
+                    Console.WriteLine("No stock was found for symbol: " + stockSymbol);
+                }
+                else
+                {
+                
+                    Console.WriteLine("\nCompany Name: " + response.Data.companyName);
+                    Console.WriteLine("Current Price: " + response.Data.latestPrice);
+                    Console.WriteLine("Year High: " + response.Data.week52High);
+                    Console.WriteLine("Year Low: " + response.Data.week52Low );
+                    Console.WriteLine("Market Cap: " + response.Data.marketCap + "\n");
+                }
+            }
+            catch (ArgumentException e)
+            {
+                Console.WriteLine("Your input cannot be blank.\n");
+            }
+            
+        }
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Stock Lookup!");
+            
+            while (true)
+            {
+                Console.WriteLine("Enter the symbol of the stock you'd like to lookup:");
+                var stockSymbol = Console.ReadLine();
+                GetStockAsync(stockSymbol).GetAwaiter().GetResult();
+                Console.WriteLine("Would you like to search again? (y/n)");
+                
+                var userInput = Console.ReadLine().ToLower();
+                if (userInput.Equals("y"))
+                {
+                    continue;
+                }  
+                else
+                {
+                    break;
+                }
+            }
+
+            Console.WriteLine("Goodbye!");
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,5 @@
+# Welcome to Stock Lookup!
+
+This is a console application that looks up stocks by symbol, and gives a brief overview of them.
+
+This takes advantage of IEX Cloud, so if you don't have an account, get one here: https://iexcloud.io/
